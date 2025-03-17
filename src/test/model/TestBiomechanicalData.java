00813b82cad7c1e6caa63787e91e1174198e6a03
Leila Mohammadi <leilafm@students.cs.ubc.ca>
2025-03-16 20:29:14 -0700

    Added tests to RiskAnalyzer class

--- src/test/model/TestBiomechanicalData.java
+++ src/test/model/TestBiomechanicalData.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 
--- src/test/model/TestRiskAnalyzer.java
+++ src/test/model/TestRiskAnalyzer.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class TestRiskAnalyzer {
     public class RiskAnalyzerTest {
